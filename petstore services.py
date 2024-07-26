a7416fe2d52953db99d66b595b24bd14fad0212e
Adithyas2003 <adathyas49@gmail.com>
2024-07-26 11:28:11 +0530

    new file added

---
+++ petstore services.py
@@ -0,0 +1,19 @@
+class pet:
+    def _init_(s):
+        print("Register")
+    def name(s):
+        print("Enter the pet name")
+    def age(s):
+        print("Enter the age")
+    def species(s):
+        print("Enter the species")
+    def __str__(s):
+        return f"{s.species}:{s.name},{s.age}"
+class Dog(pet):
+    print("Dog Details")
+    def Dog_name(s):
+        print("Enter Dog name")
+    def Dog_age(s):
+        print("Enter the Dog age")
+    def Dog
+
